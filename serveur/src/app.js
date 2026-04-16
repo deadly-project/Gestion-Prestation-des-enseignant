0187e74ef40d1550a4416d7c7904539a4214ac3b
deadly-project <johanericnathan911@gmail.com>
2026-04-16 17:32:03 +0300

    après configuration de mongodb et des fichier d'enviraronnement

--- serveur/src/app.js
+++ serveur/src/app.js
@@ -1,13 +1,40 @@
 import express from 'express'
 import pkg from 'cors'
+import 'dotenv/config'
 const cors = pkg;
+const router = express.Router()
 // const express = require('express')
 const app = express()
 const port = 3000
+const test = process.env.AUTEUR;
 app.use(cors())
-app.get('/', (req, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.status(200).json({message: "Hello world"})
-    console.log('get')
-})
+router.route('/')
+    .all((req, res, next) => {
+        next()
+    })
+    .get((req, res, next) =>{
+        res.status(200).json({message: "Get"})
+        console.log('get')
+    })
+    .post((req, res, next) =>{
+        res.status(200).json({message: "Post"})
+        console.log('post')
+    })
+    .put((req, res, next) =>{
+        res.status(200).json({message: "Put"})
+        console.log('put')
+    })
+    .delete((req, res, next) =>{
+        res.status(200).json({message: "Delete"})
+        console.log('delete')
+    })
+// app.get('/', (req, res) => {
+//     req.
+//     res.setHeader('content-type', 'application/json')
+//     res.status(200).json({message: "Hello world"})
+//     console.log('get')
+// })
 app.listen(port)
+console.log(test)
+
+
